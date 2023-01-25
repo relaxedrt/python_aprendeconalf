4c72dbd8bd3f9fe6fb85833d775bb4dc80b771fb
relaxedrt <rodrigotejeria01@gmail.com>
2023-01-25 14:57:53 +0100

    arreglar ejericico 8

--- 08Ficheros/07/Ex07.py
+++ 08Ficheros/07/Ex07.py
@@ -1,6 +1,6 @@
 #https://aprendeconalf.es/docencia/python/ejercicios/ficheros/
 import csv
-archivo = "cotizacion.csv"
+archivo = "08Ficheros/cotizacion.csv"
 dic = {}
 nombre = []
 final = []
@@ -8,21 +8,33 @@ maximo = []
 minimo = []
 volumen = []
 efectivo = []
+mf = 0
+mmax = 0
+mmin = 0
+mv = 0
+me = 0
 with open(archivo, newline='') as File:
     data = csv.reader(File)
     for row in data:
         if row != ['Nombre;Final;Máximo;Mínimo;Volumen;Efectivo']:
             ro= "".join(row)
+            ro = ro.replace(".","")
+            ro = ro.replace(",",".")
             r = ro.split(";")
             nombre.append(r[0])
-            final.append(r[1])
-            maximo.append(r[2])
-            minimo.append(r[3])
-            volumen.append(r[4])
-            efectivo.append(r[5])
+            final.append(float(r[1]))
+            maximo.append(float(r[2]))
+            minimo.append(float(r[3]))
+            volumen.append(float(r[4]))
+            efectivo.append(float(r[5]))
 dic["Nombre"] = nombre
 dic["Final"] = final
 dic["Maximo"] = maximo
 dic["Mínimo"] = minimo
 dic["Volumen"] = volumen
 dic["Efectivo"] = efectivo
+mf = sum(final)/len(final)
+mmax = sum(maximo)/len(maximo)
+mmin = sum(minimo)/len(minimo)
+mv = sum(volumen)/len(volumen)
+me = sum(efectivo)/len(efectivo)
---
+++ 08Ficheros/08/Ex08.py
@@ -0,0 +1,87 @@
+#https://aprendeconalf.es/docencia/python/ejercicios/ficheros/
+import csv
+file = "08Ficheros/calificaciones.csv"
+def examenesyasist(f):
+    with open(f, newline='') as File:
+        lista = []
+        data = csv.reader(File)
+        for row in data:
+            if row != ["Apellidos;Nombre;Asistencia;Parcial1;Parcial2;Ordinario1;Ordinario2;Practicas;OrdinarioPracticas"]:
+                row1= "".join(row)
+                #ro = row1.replace(",",".")
+                r = row1.split(";")
+                dic = {}
+                #####################################
+                dic["Apellidos"] = r[0]
+                #####################################
+                dic["Asistencia"] = r[2]
+                #####################################
+                if int(r[3]) > 10:
+                    dic["Parcial1"] = float(r[3])/10
+                else:
+                    dic["Parcial1"] = float(r[3])
+                #####################################
+                if int(r[4]) > 10:
+                    dic["Parcial2"] = float(r[4])/10
+                else:
+                    dic["Parcial2"] = float(r[4])
+                #####################################
+                if r[5] != "":
+                    if int(r[5]) > 10:
+                        dic["Ordinario1"] = float(r[5])/10
+                    else:
+                        dic["Ordinario1"] = float(r[5])
+                else:
+                    dic["Ordinario1"] = 0.0
+                #####################################
+                if r[6] != "":
+                    if int(r[6]) > 10:
+                        dic["Ordinario2"] = float(r[6])/10
+                    else:
+                        dic["Ordinario2"] = float(r[6])
+                else:
+                    dic["Ordinario2"] = 0.0
+                #####################################
+                if r[7] != "":
+                    if int(r[7]) > 10:
+                        dic["Practicas"] = float(r[7])/10
+                    else:
+                        dic["Practicas"] = float(r[7])
+                else:
+                    dic["Practicas"] = 0.0
+                #####################################
+                if r[8] != "":
+                    if int(r[7]) > 10:
+                        dic["OrdinarioPracticas"] = float(r[8])/10
+                    else:
+                        dic["OrdinarioPracticas"] = float(r[8])
+                else:
+                    dic["OrdinarioPracticas"] = 0.0
+                #####################################
+                lista.append(dic)
+    return lista
+def notafinal(l):
+    newlist = []
+    l.reverse()
+    for i in l:
+        nota = 0.0
+        if i["OrdinarioPracticas"] != 0.0 :
+            nota += (i["OrdinarioPracticas"] * 0.4)
+        else:
+            nota += (i["Practicas"] * 0.4)
+        if i["Ordinario2"] != 0.0 :
+            nota += (i["Ordinario2"] * 0.3)
+        else:
+            nota += (i["Parcial2"] * 0.3)
+        if i["Ordinario1"] != 0.0 :
+            nota += (i["Ordinario1"] * 0.3)
+        else:
+            nota += (i["Parcial1"] * 0.3)
+        i["NotaFinal"] = nota
+        newlist.append(i)
+    return newlist
+
+        
+
+
+print(notafinal(examenesyasist(file)))
